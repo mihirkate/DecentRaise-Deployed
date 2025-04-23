03fa73e142448b80456ad4145f8672b31f429d7b
mihirkate <mihirkate13@gmail.com>
2025-04-23 23:32:33 +0530

    add localhost

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,6 +5,9 @@ require("dotenv").config();
 module.exports = {
   solidity: "0.8.19",
   networks: {
+    localhost: {
+      url: "http://127.0.0.1:8545"
+    },
     hardhat: {
       chainId: 1337,
     },
